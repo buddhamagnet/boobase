5115a956b7f2ed1299fe0f7ef2bc71c3ca183245
buddhamagnet <buddhamagnet@gmail.com>
2010-01-17 19:26:15 +0000

    set title for dev/stage/live using options

--- boos.rb
+++ boos.rb
@@ -4,8 +4,21 @@ require 'erb'
 
 set :views, File.dirname(__FILE__) + '/templates'
 
+configure :production do
+  set :title, 'boobase'
+end
+
+configure :test do
+  set :title, 'boostage'
+end
+
+configure :development do
+  set :title, 'boodev'
+end
+
 before do
   @year = Time.now.year
+  @title = options.title
 end
 
 get "/" do
