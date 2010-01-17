0eab7b63a14acded9ead4b1855235e5a7dd1f773
buddhamagnet <buddhamagnet@gmail.com>
2010-01-17 20:21:34 +0000

    added API key

--- boos.rb
+++ boos.rb
@@ -6,10 +6,12 @@ set :views, File.dirname(__FILE__) + '/templates'
 
 configure :production do
   set :title, 'boobase'
+  set :api_key, 'ABQIAAAA2VaAko7JciZpUriPsbz9vxSiUcEld95qrkTs5z8_UvHFkFRNsRTXjDRbtcsTcZNhltcE7Jrr0aKJog'
 end
 
 configure :test do
   set :title, 'boostage'
+  set :api_key, 'ABQIAAAA2VaAko7JciZpUriPsbz9vxSWBiZ5gmuB-Fi-87K_EhH1x73z6BRV0VBKo7BFKBRxR9UmNf-vTU-KzA'
 end
 
 configure :development do
@@ -19,6 +21,7 @@ end
 before do
   @year = Time.now.year
   @title = options.title
+  @api_key = options.api_key
 end
 
 get "/" do
