87c00da13b00c555ca00e66722c0eab051009584
buddhamagnet <buddhamagnet@gmail.com>
2010-01-21 20:18:33 +0000

    increased DRYness and amended copy

--- public/js/boobase.js
+++ public/js/boobase.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$("#toggle_map").click(function() {
-	  $('#map').toggle("slow");
+	  $('#map1').toggle("slow");
 	});
 	$("#toggle_global_map").click(function() {
-	  $('#global_map').toggle("slow");
+	  $('#map2').toggle("slow");
 	});
 	$("#toggle_world_map").click(function() {
-	  $('#world_map').toggle("slow");
+	  $('#map3').toggle("slow");
 	});
 	
 });
