8fbbb7b43c3e152d15ebc77d93feafdfc7bac95d
buddhamagnet <buddhamagnet@gmail.com>
2010-02-06 21:55:47 +0000

    added sass stylesheet

--- boos.rb
+++ boos.rb
@@ -3,27 +3,9 @@ require 'sinatra'
 require 'feedzirra'
 require 'nokogiri'
 require 'erb'
+require 'sass'
 require 'uri'
 require 'open-uri'
-
-module Sinatra::Partials
-  def partial(template, *args)
-    template_array = template.to_s.split('/')
-    template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    options.merge!(:layout => false)
-    if collection = options.delete(:collection) then
-      collection.inject([]) do |buffer, member|
-        buffer << erb(:"#{template}", options.merge(:layout =>
-        false, :locals => {template_array[-1].to_sym => member}))
-      end.join("\n")
-    else
-      erb(:"#{template}", options)
-    end
-  end
-end
-
-include Sinatra::Partials
 
 configure do
   set :views, File.dirname(__FILE__) + '/templates'
@@ -68,6 +50,11 @@ end
 
 not_found do
   erb :index
+end
+
+get '/css/sassed.css' do
+  content_type 'text/css', :charset => 'utf_8'
+  sass :sassed
 end
 
 get "/" do
